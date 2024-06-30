32eca04c8db7b23afbb09eb272a1331cede1ab62
madaooftheblues <azlanrasool@gmail.com>
2024-06-30 21:10:20 +0500

    Add Navbar component

---
+++ src/app/constants/index.ts
@@ -0,0 +1,8 @@
+// NAVIGATION
+export const NAV_LINKS = [
+  { href: '/', key: 'home', label: 'Home' },
+  { href: '/', key: 'how_zimpak_work', label: 'How Zimpak Work?' },
+  { href: '/', key: 'services', label: 'Services' },
+  { href: '/', key: 'pricing ', label: 'Pricing ' },
+  { href: '/', key: 'contact_us', label: 'Contact Us' }
+];
